1d102a00d0e6297064cd5028dbe8a7c193bedee7
Felipe <felipecosta1961@gmail.com>
2022-03-04 18:45:55 -0300

    Esse commit envia os dados de localização para a API

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const apiKey = 'f25110b0f83adb9f7c080ee182cd1d00';
+
+navigator.geolocation.getCurrentPosition(position => {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  executarAPI(position.coords.latitude, position.coords.longitude);
+});
+
+function executarAPI(latitude, longitude) {
+  const promessa = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  );
+
+  promessa.then(dados => {
+    console.log(dados);
+  });
+
+  promessa.catch(erro => {});
+}
