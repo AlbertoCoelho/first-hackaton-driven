d7b795644a00f82c16dbb5120f60c85af56464f2
Felipe <felipecosta1961@gmail.com>
2022-03-04 19:59:08 -0300

    Esse commit pergunta o nome da cidade do usuário caso o serviçode localização não seja aceito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,44 @@
 const apiKey = 'f25110b0f83adb9f7c080ee182cd1d00';
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            console.log(position.coords.latitude);
-            console.log(position.coords.longitude);
-            executarAPI(position.coords.latitude, position.coords.longitude);
-        },
-        () => {
-            const latitude = prompt('Qual é a latitude?');
-            const longitude = prompt('Qual é a longitude?');
-            executarAPI(latitude, longitude);
-        }
-    );
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      pegarDadosAPI(position.coords.latitude, position.coords.longitude);
+    },
+    () => {
+      const cidade = prompt('Qual é a sua cidade?');
+      pegarDadosCidadeAPI(cidade);
+    }
+  );
 }
 
-function executarAPI(latitude, longitude) {
-    const promessa = axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
-    );
+function pegarDadosAPI(latitude, longitude) {
+  const promessa = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  );
 
-    promessa.then(dados => {
-        console.log(dados);
-    });
+  promessa.then(dados => {
+    console.log(dados);
+  });
 
-    promessa.catch(erro => {
-        const latitude = prompt('Qual é a latitude?');
-        const longitude = prompt('Qual é a longitude?');
-        executarAPI(latitude, longitude);
-    });
+  promessa.catch(erro => {});
 }
 
+function pegarDadosCidadeAPI(cidade) {
+  console.log(cidade);
+  const promessa = axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`
+  );
 
+  promessa.then(dados => {
+    console.log(dados);
+  });
 
+  promessa.catch(() => {
+    alert('Digite um nome de cidade válido');
+    const cidade = prompt('Qual é a sua cidade?');
+    pegarDadosCidadeAPI(cidade);
+  });
+}
